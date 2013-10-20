9fa2e17dc8bd60ae82e6038ca13cbb0e2e3f3f1a
jpmilcent <jpm@clapas.org>
2013-10-20 15:12:37 +0200

    Suppression composant de test.

--- web/components/click.dart
+++
@@ -1,7 +0,0 @@
-import 'package:polymer/polymer.dart';
-
-@CustomTag('click-counter')
-class CounterComponent extends PolymerElement with ObservableMixin {
-	@observable int count = 0;
-	void increment(e) { count++; }
-}
