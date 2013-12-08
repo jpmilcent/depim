68e344478b10ddb0d51ac6a80c01356be9f714c6
jpmilcent <jpm@clapas.org>
2013-12-08 18:16:19 +0100

    Utilisation de @MirrorsUsed. Permet d'éviter le bug "noSuchMethodError : method not found..." lors du déploiment en JS

--- web/lib/components/breadcrumb.dart
+++ web/lib/components/breadcrumb.dart
@@ -1,12 +1,15 @@
 import 'package:polymer/polymer.dart';
 import 'dart:html';
 import '../models/BreadcrumbElement.dart';
+import 'dart:mirrors';
 
 @CustomTag('app-breadcrumb')
-class Breadcrumb extends PolymerElement with Observable {
+class Breadcrumb extends PolymerElement {
 
 	bool get applyAuthorStyles => true;
-	@observable List breadcrumb = toObservable([]);
+	// INFO : MirrorsUsed nécessaire pour éviter le bug "noSuchMethodError : method not found" lors du déploiment en JS
+	@MirrorsUsed(targets: 'Element,Node', symbols: 'name,href,classCSS,divider')
+	@observable ObservableList breadcrumb = toObservable([]);
 
 	Map<String, String> entryPath = <String, String>{
 		'Accueil': '#home',
@@ -15,34 +18,44 @@ class Breadcrumb extends PolymerElement with Observable {
 	};
 
 	Map<String, List> siteMap = <String, List>{
-		'': ['Accueil'],
 		'#home': ['Accueil'],
 		'#warehouse': ['Accueil', 'Dépots'],
 		'#doc': ['Accueil', 'Documents']
 	};
+	
+	Breadcrumb.created() : super.created();
 
-	Breadcrumb.created() : super.created() {
-		print('Hash:'+window.location.hash);
-		changeBreadcrumb(window.location.hash);
-		window.onHashChange.listen((_) {
-			changeBreadcrumb(window.location.hash);
-		});
+	void enteredView() {
+	  super.enteredView();
+	  print('Hash:'+window.location.hash);
+		
+		window
+		  ..onLoad.listen((_) {changeBreadcrumb(window.location.hash);})
+		  ..onHashChange.listen((_) {changeBreadcrumb(window.location.hash);});
+		if (window.location.hash == '') {
+		  window.location.hash = '#home';
+		}
 	}
 
-	changeBreadcrumb(id) {
-		print('BC id : $id');
-		
-		var breadcrumbLength = siteMap[id].length;
-		breadcrumb.clear();
-		for (var i = 0; i < breadcrumbLength; i++) {
-			var entry = siteMap[id][i],
-			position = i + 1;
-			var breadcrumbElemt = new BreadcrumbElement();
-			breadcrumbElemt.name = entry;
-			breadcrumbElemt.href = (entryPath[entry] != null) ? entryPath[entry] : '';
-			breadcrumbElemt.classCss = (position == breadcrumbLength) ? 'active' : '';
-			breadcrumbElemt.divider = (position != breadcrumbLength);
-			breadcrumb.add(breadcrumbElemt);
+	void changeBreadcrumb(String id) {
+	  print('BC id : $id');
+		if (siteMap.containsKey(id)) {
+  		var breadcrumbLength = siteMap[id].length;
+  		print('BC breadcrumbLength : $breadcrumbLength');
+  		breadcrumb.clear();
+  		print('BC after clear');
+  		for (var i = 0; i < breadcrumbLength; i++) {
+  			var entry = siteMap[id][i],
+          position = i + 1,
+          breadcrumbElemt = new BreadcrumbElement();
+        
+  			breadcrumbElemt.name = entry;
+  			breadcrumbElemt.href = (entryPath[entry] != null) ? entryPath[entry] : null;
+  			breadcrumbElemt.classCss = (position == breadcrumbLength) ? 'active' : null;
+  			breadcrumbElemt.divider = (position != breadcrumbLength);
+  			print('BC before add :'+breadcrumbElemt.toString());
+  			breadcrumb.add(breadcrumbElemt);
+  		}
 		}
 	}
 }
--- web/lib/models/BreadcrumbElement.dart
+++ web/lib/models/BreadcrumbElement.dart
@@ -1,6 +1,10 @@
-class BreadcrumbElement {
+class BreadcrumbElement extends Object {
 	String name = '';
 	String classCss = '';
 	String href = '';
 	bool divider = false;
+	
+	String toString() {
+	  return 'name=$name;classCss=$classCss;href=$href;divider=$divider';
+	}
 }
